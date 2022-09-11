1b7c1b4f9b5ed4bd9e20661ba5946f41003ea792
eliot <eliotkimmelcentillion@gmail.com>
2022-09-10 22:10:07 -0500

    see if sprites show up

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Control : MonoBehaviour
 {
-    public Rigidbody2D myRigidBody2D;
+    private Rigidbody2D myRigidBody2D;
+    public float controlIntensity = 1000f;
+    void Start() {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+    }
     void Update()
     {
-        float forceX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * 4000f;
-        float forceY = Input.GetAxisRaw("Vertical") * Time.deltaTime * 4000f;
+        float forceX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * controlIntensity;
+        float forceY = Input.GetAxisRaw("Vertical") * Time.deltaTime * controlIntensity;
 
         myRigidBody2D.AddForce(new Vector2(forceX, forceY));
     }
--- Assets/Scripts/NewtonianGravity.cs
+++ Assets/Scripts/NewtonianGravity.cs
@@ -2,22 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// gravitates toward other objects by the inverse square law
 [RequireComponent(typeof(Rigidbody2D))]
 [RequireComponent(typeof(Collider2D))]
 public class NewtonianGravity : MonoBehaviour
 {
     // mass is built into rigidBody2D
     const float gravConstant = 20f;
-    public Rigidbody2D myRigidBody2D;
-    public Collider2D myCollider2D;
+    private Rigidbody2D myRigidBody2D;
+    private Collider2D myCollider2D;
     private float detectionRadius;
     // Start is called before the first frame update
     void Start() {
-        detectionRadius = myRigidBody2D.mass * gravConstant * 2f;
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+        myCollider2D = GetComponent<Collider2D>();
+        detectionRadius = gravConstant * 0.2f;
     }
 
     void Update() {
-        // TODO: make a collision layer for newtonian objects
         // finds all colliders in our radius
         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, detectionRadius);
         // for each collider in our radius, gravitate toward it
@@ -26,8 +28,12 @@ public class NewtonianGravity : MonoBehaviour
             if (collider.transform == transform) continue;
             Vector2 vectorDifference = (collider.transform.position - transform.position);
             float distance = vectorDifference.magnitude;
-            float gravForce = gravConstant * (myRigidBody2D.mass * collider.attachedRigidbody.mass) / (distance * distance);
+            float gravForce = gravConstant * (myRigidBody2D.mass * collider.attachedRigidbody.mass) / (distance * Mathf.Sqrt(distance));
             myRigidBody2D.AddForce(vectorDifference.normalized * gravForce * Time.deltaTime);
         }
+    }
+
+    void OnDrawGizmos() {
+        UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.back, detectionRadius);
     }
 }
