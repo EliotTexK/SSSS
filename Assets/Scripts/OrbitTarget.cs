1d63cc13cc20997d0e312b4c1a443e14c20eab91
eliot <eliotkimmelcentillion@gmail.com>
2022-09-12 19:32:50 -0500

    planets are now spawned and physics can be turned on/off

--- Assets/Scripts/OrbitTarget.cs
+++ Assets/Scripts/OrbitTarget.cs
@@ -6,48 +6,56 @@ using UnityEngine;
 [RequireComponent(typeof(CircleCollider2D))]
 public class OrbitTarget : MonoBehaviour
 {
+    public bool updatePhysics = false;
     public Transform target;
-    public float density = 0.1f;
     private float mass;
     public Vector2 velocity;
-    const float gravConstant = 4f;
+    const float gravConstant = 15f;
     // adjust orbit to be stable and circular
-    public float adjustOrbitIntensity = 10f;
+    public float adjustOrbitIntensity = 2f;
+    // the adjustment value decreases as time goes on
+    public float adjustActual;
     // Start is called before the first frame update
     void Start() {
         CircleCollider2D myCollider = GetComponent<CircleCollider2D>();
-        mass = density * Mathf.PI * myCollider.radius * myCollider.radius;
+        mass = Mathf.PI * myCollider.radius * myCollider.radius;
         // give an initial velocity that will stabilize orbit
-        Vector2 targetDir = (target.position - transform.position);
-        float distance = targetDir.magnitude;
-        Vector2 perpendicularNorm = Vector2.Perpendicular(targetDir).normalized;
-        velocity = perpendicularNorm * Mathf.Sqrt(gravConstant * mass / distance) * 0.01f;
-        // apply gravitational acceleration, too
-        float gravScalar = gravConstant * mass / (distance * distance);
-        Vector2 gravAccel = targetDir.normalized * gravScalar;
-        velocity += gravAccel;
+        applyInitialForce();
+        adjustActual = adjustOrbitIntensity * 4;
     }
 
     void FixedUpdate() {
-        if (target)
-        {
-            // apply gravitational acceleration
-            Vector2 targetDir = (target.position - transform.position);
-            float distance = targetDir.magnitude;
-            float gravScalar = gravConstant * mass / (distance * distance);
-            Vector2 gravAccel = targetDir.normalized * gravScalar;
-            velocity += gravAccel * Time.fixedDeltaTime;
-            // adjust orbit
-            Vector2 perpendicularNorm = Vector2.Perpendicular(targetDir).normalized;
-            Vector2 velCentrip = Vector2.Dot(velocity, perpendicularNorm) * perpendicularNorm;
-            Vector2 velTowards = Vector2.Dot(velocity, targetDir.normalized) * targetDir.normalized;
-            // add centripetal
-            velocity += velCentrip * velTowards.magnitude * Time.fixedDeltaTime * adjustOrbitIntensity;
-            velocity -= velTowards * Time.fixedDeltaTime * adjustOrbitIntensity;
-            DrawPhysicsVector(velCentrip, 200f);
-            DrawPhysicsVector(velTowards, 200f);
+        if (updatePhysics) {
+            if (target)
+            {
+                // apply gravitational acceleration
+                Vector2 targetDir = (target.position - transform.position);
+                float distance = targetDir.magnitude;
+                float gravScalar = gravConstant * mass / (distance * distance);
+                Vector2 gravAccel = targetDir.normalized * gravScalar;
+                velocity += gravAccel * Time.fixedDeltaTime;
+                // adjust orbit
+                Vector2 perpendicularNorm = Vector2.Perpendicular(targetDir).normalized;
+                Vector2 velCentrip = Vector2.Dot(velocity, perpendicularNorm) * perpendicularNorm;
+                Vector2 velTowards = Vector2.Dot(velocity, targetDir.normalized) * targetDir.normalized;
+                // add centripetal
+                velocity += velCentrip * velTowards.magnitude * Time.fixedDeltaTime * adjustActual;
+                velocity -= velTowards * Time.fixedDeltaTime * adjustActual;
+            }
+            // adjust orbit more during the first few seconds of gameplay
+            if (adjustActual > adjustOrbitIntensity/4) {
+                adjustActual -= Time.fixedDeltaTime;
+            }
+            transform.Translate(velocity);
         }
-        transform.Translate(velocity);
+    }
+
+    private void applyInitialForce() {
+        Vector2 targetDir = (target.position - transform.position);
+        float distance = targetDir.magnitude;
+        Vector2 perpendicularNorm = Vector2.Perpendicular(targetDir).normalized;
+        float gravForce = gravConstant * mass / (distance * distance);
+        velocity = perpendicularNorm * (0.1f + 0.05f * gravForce);
     }
 
     private void DrawPhysicsVector(Vector2 vector, float scale) {
